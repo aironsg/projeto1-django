c6e474aeedcd6561bae3a5402eb5710723ff7efa
airon <airon_gm@hotmail.com>
2022-03-16 15:36:49 -0300

    criando primeira base de dados(teste)

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+"""
+    Classe Recipe será transforma em uma tabela dentro da base de dados
+"""
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=180)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=50)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=50)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
