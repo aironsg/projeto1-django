7a91aa55830aab5dde62c06db30b939be7e76a6e
airon <airon_gm@hotmail.com>
2022-03-17 11:51:54 -0300

    buscando receitas da base de dados(teste)

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Category, Recipe
+
+
 # Register your models here.
+# forma 1 de fazer o registro
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
+
+# forma 2 de fazer o registro
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
--- recipes/models.py
+++ recipes/models.py
@@ -12,6 +12,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -31,3 +34,6 @@ class Recipe(models.Model):
         Category, on_delete=models.SET_NULL, null=True
     )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
