ddf6620160f5dac6072d3ec25657134f4a6bea9c
airon <airon_gm@hotmail.com>
2022-03-18 10:54:00 -0300

    adicionando permissao de html no modo de preparo das receitas

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 # from django.http import Http404
 from django.shortcuts import get_list_or_404, render
-from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
 
@@ -35,9 +34,15 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
 
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
 
     })
