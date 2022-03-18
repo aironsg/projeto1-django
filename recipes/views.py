e14635a1df3ede86ab9044e510334d7796913eeb
airon <airon_gm@hotmail.com>
2022-03-17 21:19:46 -0300

    adicionando busca por categorias especificas e tratamento de pagina nao encontrada

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
@@ -14,8 +15,12 @@ def home(request):
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Pagina não encontrada 🥲')
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - |Categorias|'
     })
 
 
